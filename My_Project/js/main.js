a0a1d4dd6acf93dab416f687ad433d8609867144
TheAnswer88 <magicnity@gmail.com>
2019-01-17 20:49:08 +0300

    r

--- My_Project/js/main.js
+++ My_Project/js/main.js
@@ -16,3 +16,16 @@ $('.stop').on('click',function(){
 })
 });
 new WOW().init();
+$(window).on('load', function () {
+    $preloader = $('.loaderArea'),
+      $loader = $preloader.find('.loader');
+    $loader.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+  });
+$(document).ready(function() {
+		$(".main_menu_button").click(function() {
+		$(".menu li").slideToggle("slow");
+	});
+});
+
+
